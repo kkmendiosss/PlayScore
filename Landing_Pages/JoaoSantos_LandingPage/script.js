64881314aed19b4353b3b8cb81456325ddb05d52
jmlvp <2231974@my.ipleiria.pt>
2026-03-20 22:43:11 +0000

    a

---
+++ Landing_Pages/JoaoSantos_LandingPage/script.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    const ctaButton = document.querySelector('a[href="#features"]');
+    if (ctaButton) {
+        ctaButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+    const form = document.getElementById('signup-form');
+    if (form) {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const userName = document.getElementById('name').value;
+
+            alert(`Conta criada com sucesso, ${userName}! O teu cupão Humble Bundle será enviado para o teu email.`);
+
+            form.reset();
+        });
+    } else {
+        console.error("Erro: O formulário com id 'signup-form' não foi encontrado no HTML.");
+    }
+};
