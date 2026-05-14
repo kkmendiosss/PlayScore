05ba54fb7893d4e0128697dd8fedd908032cce68
Joao Mendes <2242574@my.ipleiria.pt>
2026-05-14 17:32:06 +0100

    a

---
+++ Site/js/jogo.js
@@ -0,0 +1,13 @@
+const loginBtn = document.querySelector(".login-btn");
+
+loginBtn.addEventListener("click", () => {
+    alert("Página de login em desenvolvimento!");
+});
+
+const favoriteBtn = document.querySelector(".favorite");
+
+favoriteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    favoriteBtn.innerText = "Adicionado aos favoritos!";
+});
