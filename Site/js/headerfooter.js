074e37c3dcdfecbda731f8c6c96b167d9a30df56
Rodas79 <2242573@my.ipleiria.pt>
2026-06-28 15:49:53 +0100

    a

--- Site/js/headerfooter.js
+++ Site/js/headerfooter.js
@@ -4,3 +4,16 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+// Dropdowns no telemóvel
+document.querySelectorAll(".dropdown > a").forEach(link => {
+    link.addEventListener("click", function (e) {
+
+        if (window.innerWidth <= 768) {
+            e.preventDefault();
+
+            this.parentElement.classList.toggle("active");
+        }
+
+    });
+});
